7bd23896176f5291624176bf220c4187b28fad31
Ibtesum <aninda7152@gmail.com>
2022-03-18 02:58:05 +0600

    First Commit

---
+++ mysite/main/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import URLPattern, path
+
+from . import views
+
+urlpatterns = [
+    path("<int:id>", views.index, name = "index"),
+    path("", views.home, name="home")
+]
---
+++ mysite/main/views.py
@@ -0,0 +1,13 @@
+from unicodedata import name
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList, Item
+# Create your views here.
+
+def index(response, id):
+
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/base.html", {})
+
+def home(response):
+    return render(response, "main/home.html", {})
